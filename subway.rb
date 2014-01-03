75fe48277f8006305187cb73fcd52d586e1d7623
Rae Farine <rfarine1027@gmail.com>
2014-01-03 15:03:28 -0500

    Fix

--- subway.rb
+++ subway.rb
@@ -5,7 +5,5 @@ init.display_station_list!
 start = init.get_starting_station!
 puts "Hi there I am in subwayrb and my start is #{start}"
 
-
-
 #final = init.get_final_station!
 #init.give_directions(start, final)
