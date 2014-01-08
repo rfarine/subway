d46685ed562aeef503c2bef421704b953fee5e88
Rae Farine <rfarine1027@gmail.com>
2014-01-08 17:48:45 -0500

    Got the tests to pass

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -6,12 +6,12 @@ describe 'Station' do
 	end
 
 	it 'should initialize with train line' do
-		station = Station.new(:line => 'L')
+		station = Station.new
 		station.get_my_line.should eq('L')
 	end
 
 	it 'should initialize with station name' do
-		station = Station.new(:name => 'Grand Street')
+		station = Station.new
 		station.get_my_name.should eq('Grand Street')
 	end
 end
