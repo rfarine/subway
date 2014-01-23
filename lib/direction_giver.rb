68b4a6fef874a288525db969644d7d30246bae63
Rae Farine <rfarine1027@gmail.com>
2014-01-23 16:23:18 -0500

    Got transfer instructions to pass test... need to clean up a lot.

--- lib/direction_giver.rb
+++ lib/direction_giver.rb
@@ -31,22 +31,32 @@ class DirectionGiver
 
 		case start_line_1
 		when "L"
+			start_station = Station.new(start_name_1,start_line_1)
+			transfer_station = Station.new("Sixth Avenue (14th Street)","F")
 			start_line_2 = "F" # Line to transfer to
 			transfer_name = "Sixth Avenue (14th Street)" # Transfer station
-			transfer_entry = @stations.select { |number,stop| stop[0] == transfer_name } # Find the transfer station in the list of stations
-			direction_1 = direction_on_l(start, transfer_entry) # Direction to ride L to transfer station
-			number_of_stops_1 = number_of_stops(start, transfer_entry)
+			start_name_2 = "14th Street (Sixth Avenue)"
+			start_station_2 = Station.new(start_name_2, start_line_2)
+			direction_1 = direction_on_l(start_station, transfer_station) # Direction to ride L to transfer station
+			direction_2 = direction_on_f(start_station_2,final)
+			number_of_stops_1 = number_of_stops(start_station, transfer_station)
+			number_of_stops_2 = number_of_stops(start_station_2,final)
 		when "F"
-			start_line_2 = "L"
+			start_station = Station.new(start_name_1,start_line_1)
+			transfer_station = Station.new("14th Street (Sixth Avenue)", "L")
+			start_line_2 = "L" # Line to transfer to
 			transfer_name = "14th Street (Sixth Avenue)"
-			transfer_entry = @stations.select { |number,stop| stop[0] == transfer_name } # Find the transfer station in the list of stations
-			direction_1 = direction_on_f(start, transfer_entry) # Direction to ride L to transfer station
-			number_of_stops_1 = number_of_stops(start, transfer_entry)
+			start_name_2 = "Sixth Avenue (14th Street)"
+			start_station_2 = Station.new(start_name_2, start_line_2)
+			direction_1 = direction_on_f(start_station, transfer_station) # Direction to ride L to transfer station
+			direction_2 = direction_on_l(start_station_2,final)
+			number_of_stops_1 = number_of_stops(start_station, transfer_station)
+			number_of_stops_2 = number_of_stops(start_station_2,final)
 		else
 			raise
 		end
 
-		instructions = "Get on the #{start_line_1} at #{start_name_1} and take the train #{direction_1} for #{number_of_stops_1} and get off at #{transfer_name}. Then, get on the #{start_line_2} at #{start_name_2}, ride the train #{direction_2} for #{number_of_stops_2} stops and get off at #{final_name_2}."
+		instructions = "Get on the #{start_line_1} at #{start_name_1} and take the train #{direction_1} for #{number_of_stops_1} stops and get off at #{transfer_name}. Then, get on the #{start_line_2} at #{start_name_2}, ride the train #{direction_2} for #{number_of_stops_2} stops and get off at #{final_name_2}."
 		return instructions
 	end
 
