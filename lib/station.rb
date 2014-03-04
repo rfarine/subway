e22fc54277673ffbbd719c090dc15cbad2fc2272
Rae Farine <rfarine1027@gmail.com>
2014-03-04 17:48:22 -0500

    Split up station list.

--- lib/station.rb
+++ lib/station.rb
@@ -7,8 +7,15 @@ class Station
 	end
 
 	def self.list_stations
+		stations = [l_stations, f_stations, g_stations]
+		stations.each_with_object(Hash.new([])) do |h1, h|
+			h1.each { |key, value| h[key] = h[key] + value }
+		end
+	end
+
+	def self.l_stations
 		{
-			"1" => ["Grand Street", "L"], ### L stations start
+			"1" => ["Grand Street", "L"],
 			"2" => ["Graham Avenue", "L"],
 			"3" => ["Lorimer Street", "L"],
 			"4" => ["Bedford Avenue", "L"],
@@ -16,9 +23,13 @@ class Station
 			"6" => ["Third Avenue", "L"],
 			"7" => ["14th Street/Union Square", "L"],
 			"8" => ["Sixth Avenue (14th Street)", "L"], ## This is where F & L intersect
-			"9" => ["Eighth Avenue", "L"],
+			"9" => ["Eighth Avenue", "L"]
+		}
+	end
 
-			"10" => ["Second Avenue", "F"], ### F stations start
+	def self.f_stations
+		{
+			"10" => ["Second Avenue", "F"],
 			"11" => ["Broadway-Lafayette Street", "F"],
 			"12" => ["West Fourth Street", "F"],
 			"13" => ["14th Street (Sixth Avenue)", "F"], ## This is where F & L intersect
@@ -26,9 +37,13 @@ class Station
 			"15" => ["34th Street-Herald Square", "F"],
 			"16" => ["42nd Street-Bryant Park", "F"],
 			"17" => ["47-50 Streets-Rockefeller Center", "F"],
-			"18" => ["57th Street", "F"],
+			"18" => ["57th Street", "F"]
+		}
+	end
 
-			"19" => ["Greenpoint Avenue", "G"], ### G stations start
+	def self.g_stations
+		{
+			"19" => ["Greenpoint Avenue", "G"],
 			"20" => ["Nassau Avenue", "G"],
 			"21" => ["Metropolitan Avenue", "G"], ### This is where G & L intersect
 			"22" => ["Broadway", "G"],
@@ -39,4 +54,5 @@ class Station
 			"27" => ["Clinton-Washington Avenues", "G"]
 		}
 	end
+
 end
